62af2d912237436cadc6202f2a65eaa685ffc8ba
VI25 <Vicho255@users.noreply.github.com>
2023-04-06 15:30:24 -0400

    Variables CI

---
+++ Variable.py
@@ -0,0 +1,24 @@
+print ("Hola a todos")
+
+###Variable nombre
+nombre="Vicente"
+
+#Impresion de una variable
+print ("Hola a todos, me llamo", nombre)
+
+#declarando una tercera variable
+edad="18 años"
+
+#impresion a la segunda varibale
+print ("Mi edad es de", edad)
+
+#Imp
+# intento print("hola mi nombre es", + nombre + "y tengo", edad + "años")
+print ("Hola a todos, mi nombre es", str(nombre), "y tengo", str(edad))
+#actu de nombre
+nombre2= "Pedro"
+print ("yo soy", nombre2)
+#utilizando la instruccion input
+#a
+nombre2= input ("¿cual es tu nombre?\n")
+print("Tu nombre es", nombre2)
