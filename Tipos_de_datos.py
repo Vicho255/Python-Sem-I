c56d58a15f1227df58630a9e7368932551480f86
VI25 <Vicho255@users.noreply.github.com>
2023-04-06 15:59:46 -0400

    Tipos de datos CII

---
+++ Tipos_de_datos.py
@@ -0,0 +1,27 @@
+#01. Datos de tipo Numerico
+estatura = 1.70
+peso = 87
+complejo = 1 +4j
+
+print("Imprecion del numero complejo", complejo)
+
+#Operacion aritmetica
+imc = peso/estatura**2
+print("Mi IMC es de:", imc)
+
+#02. Datos de tipo cadena de caracteres
+asisgnatura = "Programacion"
+carrera = "Ingieneria Civil Informatica"
+
+#03. Datos de tipo booleanos
+ampolleta = False
+interruptor = True
+
+#Con type podemos saver que tipo de dato estamos tratando
+print(type(ampolleta))
+
+#04. Datos de tipo Array (Objetos de tipo coleccion)
+estudiantes = ["Matias", "Carlos", "Miguel", "Cristobal"]
+num = [1, 2, 3, 4, 5, 6]
+print(estudiantes)
+print(num)
