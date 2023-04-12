2a08d213e92efb54f46e8c4b049ac04223a1d4c2
VI25 <Vicho255@users.noreply.github.com>
2023-04-12 17:55:42 -0400

    Clase 4

---
+++ RetoClaseI.py
@@ -0,0 +1,7 @@
+edad = input("¿Cual es tu edad?")
+nombre = input("¿Cual es tu nombre?")
+n = int(edad) + 20
+
+print("Hola mi nombre es", nombre, "tengo", edad, "y en 20 años tendre", n, "años")
+#Otro modo de hacerlo
+print("Hola mi nombre es", nombre, "tengo", edad, "y en 20 años tendre", int(edad) + 20, "años")
